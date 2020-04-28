3b068c44dc1f5fad9839feeb25e250f9167284b7
Hiro281 <44799467+Hiro281@users.noreply.github.com>
2020-04-28 12:29:11 +0900

    3-9

--- 978-4-7980-5347-9/MyBootApp/src/main/java/com/tuyano/springboot/HelloController.java
+++ 978-4-7980-5347-9/MyBootApp/src/main/java/com/tuyano/springboot/HelloController.java
@@ -1,14 +1,22 @@
 package com.tuyano.springboot;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController
 {
-    @RequestMapping("/")
-    public String index()
+    @RequestMapping("/{Number}")
+    public String index(@PathVariable int Number)
     {
-        return "Hello Spring-Boot World!";
+        int intResolution = 0;
+        
+        for (int i = 1; i <= Number; i++)
+        {
+            intResolution += i;
+        }
+        
+        return "Total: " + intResolution;
     }
 }
