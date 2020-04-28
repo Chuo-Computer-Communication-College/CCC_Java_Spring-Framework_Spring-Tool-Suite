819b2368822edd00065437b3f34836d3e173c77a
Hiro281 <44799467+Hiro281@users.noreply.github.com>
2020-04-28 15:58:01 +0900

    List 3-13

--- 978-4-7980-5347-9/MyBootApp/src/main/java/com/tuyano/springboot/HelloController.java
+++ 978-4-7980-5347-9/MyBootApp/src/main/java/com/tuyano/springboot/HelloController.java
@@ -1,33 +1,14 @@
 package com.tuyano.springboot;
 
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController
-{
-    String[] name_a = 
-                        {
-                            "tuyano", 
-                            "hanako", 
-                            "taro", 
-                            "sachiko", 
-                            "ichiro"
-                        };
-
-    String[] mail_a = 
-                        {
-                            "shoda@tuuyano.com", 
-                            "hanako@flower", 
-                            "taro@yamada", 
-                            "sachiko@happy", 
-                            "ichiro@baseball"
-                        };
-    
-    @RequestMapping("/{id}")
-    public DataObject index(@PathVariable int id)
+{    
+    @RequestMapping("/")
+    public String index()
     {        
-        return new DataObject(id, name_a[id], mail_a[id]);
+        return "index";
     }
 }
