479b076d1cf8ca8f04f05fb16fbe22f61bb53bbb
Hiro281 <44799467+Hiro281@users.noreply.github.com>
2020-05-01 15:26:31 +0900

    List 4-22

--- 978-4-7980-5347-9/MyBootApp/src/main/java/com/tuyano/springboot/HelloController.java
+++ 978-4-7980-5347-9/MyBootApp/src/main/java/com/tuyano/springboot/HelloController.java
@@ -3,7 +3,6 @@ package com.tuyano.springboot;
 import java.util.ArrayList;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
