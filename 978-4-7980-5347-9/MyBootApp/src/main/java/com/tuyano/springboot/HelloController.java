e73e4925a8b247e0dcd3adcf4ab935ab045cde7d
Hiro281 <44799467+Hiro281@users.noreply.github.com>
2020-05-12 15:22:38 +0900

    List 5-13

--- 978-4-7980-5347-9/MyBootApp/src/main/java/com/tuyano/springboot/HelloController.java
+++ 978-4-7980-5347-9/MyBootApp/src/main/java/com/tuyano/springboot/HelloController.java
@@ -2,7 +2,10 @@ package com.tuyano.springboot;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.tuyano.springboot.repositories.MyDataRepository;
@@ -13,16 +16,25 @@ public class HelloController
     @Autowired
     MyDataRepository mdRepository;
     
-    @RequestMapping("/")
-    public ModelAndView index(ModelAndView mav)
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView index(ModelAndView mav, @ModelAttribute("formModel") MyData md)
     {
         Iterable<MyData> itrMyData = mdRepository.findAll();
 
-        mav.addObject("message", "This is sample content.");
-        mav.addObject("data", itrMyData);
+        mav.addObject("Message", "This is sample content.");
+        mav.addObject("DataList", itrMyData);
         
         mav.setViewName("index");
         
         return mav;
+    }
+    
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    @Transactional(readOnly = false)
+    public ModelAndView form(ModelAndView mav, @ModelAttribute("formModel") MyData md)
+    {
+        mdRepository.saveAndFlush(md);
+        
+        return new ModelAndView("redirect:/");
     }
 }
