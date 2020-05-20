5327601ab8a37e08fc3cafaa0f4c7193d64c10b9
Hiro281 <44799467+Hiro281@users.noreply.github.com>
2020-05-20 14:24:37 +0900

    List 6-26

--- 978-4-7980-5347-9/MyBootApp/src/main/java/com/tuyano/springboot/MyDataDaoImpl.java
+++ 978-4-7980-5347-9/MyBootApp/src/main/java/com/tuyano/springboot/MyDataDaoImpl.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -15,7 +17,7 @@ public class MyDataDaoImpl implements MyDataDao<MyData>
     private static final long SERIAL_VERSION_UID = 1L;
 
     @PersistenceContext
-    private EntityManager eManager;
+    EntityManager eManager;
     
     @Autowired
     MyDataDaoImpl mdDAO; 
@@ -23,14 +25,46 @@ public class MyDataDaoImpl implements MyDataDao<MyData>
     @Override
     public List<MyData> getAll()
     {
-        Query qry = eManager.createQuery("FROM MyData");
+        // 取り出す位置の指定
+        int intOffset = 1;
+        // 取り出す個数の指定
+        int intLimit = 2;
         
-        @SuppressWarnings("unchecked")
-        List<MyData> lstMyData = qry.getResultList();
+        CriteriaBuilder cBuilder = eManager.getCriteriaBuilder();
         
-        eManager.close();
+        CriteriaQuery<MyData> cQueryMyData = cBuilder.createQuery(MyData.class);
+        
+        Root<MyData> rootMyData = cQueryMyData.from(MyData.class);
+        
+        cQueryMyData.select(rootMyData);
+        
+        List<MyData> lstMyData = (List<MyData>)eManager.createQuery(cQueryMyData).setFirstResult(intOffset).setMaxResults(intLimit).getResultList();
         
         return lstMyData;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<MyData> find(String fstr)
+    {
+        CriteriaBuilder cBuilder = eManager.getCriteriaBuilder();
+        
+        CriteriaQuery<MyData> cqMyData = cBuilder.createQuery(MyData.class);
+        
+        Root<MyData> rMyData = cqMyData.from(MyData.class);
+        
+        cqMyData.select(rMyData).where(cBuilder.equal(rMyData.get("name"), fstr));
+        
+        List<MyData> lstMyData = (List<MyData>)eManager.createQuery(cqMyData).getResultList();
+        
+        return lstMyData;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<MyData> findByAge(int min, int max)
+    {
+        return (List<MyData>)eManager.createNamedQuery("findByAge").setParameter("min", min).setParameter("max", max).getResultList();
     }
 
     @Override
