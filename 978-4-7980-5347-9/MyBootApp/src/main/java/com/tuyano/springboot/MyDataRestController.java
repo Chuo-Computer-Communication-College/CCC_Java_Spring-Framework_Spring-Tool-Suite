f06a284316add70bdc01de1f296ce9a538c7f27a
Hiro281 <44799467+Hiro281@users.noreply.github.com>
2020-05-21 15:57:09 +0900

    List 7-3

---
+++ 978-4-7980-5347-9/MyBootApp/src/main/java/com/tuyano/springboot/MyDataRestController.java
@@ -0,0 +1,27 @@
+package com.tuyano.springboot;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyDataRestController
+{
+    @Autowired
+    private MyDataService mdService;
+    
+    @RequestMapping("/rest")
+    public List<MyData> restAll()
+    {
+        return mdService.getAll();
+    }
+    
+    @RequestMapping("/rest/{number}")
+    public MyData restBy(@PathVariable int number)
+    {
+        return mdService.get(number);
+    }
+}
